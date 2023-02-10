2d440930ef68ebb747f64cdd5434410052a25672
woong <woongdam777@gmail.com>
2023-02-10 17:48:44 +0900

    20230210_반복문for연습문제

--- 01_Java/a003_control/src/edu/kh/control/loop/ex/ForRun.java
+++ 01_Java/a003_control/src/edu/kh/control/loop/ex/ForRun.java
@@ -19,7 +19,17 @@ public class ForRun {
 //		 fx.ex11();
 //		 fx.ex12();
 //		 fx.ex13();
-		 fx.ex14();
+//		 fx.ex14();
+		
+		//중첩 반복문
+//		 fx.ex15();
+//		 fx.ex16();
+//		 fx.ex17();
+//		 fx.ex18();
+//		 fx.ex19();
+//		 fx.ex20();
+//		 fx.ex21();
+		 fx.ex22();
 		
 	}
 }
---
+++ 01_Java/a003_control/src/edu/kh/control/loop/practice/ForPracticeRun.java
@@ -0,0 +1,21 @@
+package edu.kh.control.loop.practice;
+
+public class ForPracticeRun {
+
+	public static void main(String[] args) {
+		
+		ForPractice fp = new ForPractice();
+		
+//		fp.practice1();
+//		fp.practice2();
+//		fp.practice3();
+//		fp.practice4();
+//		fp.practice5();
+//		fp.practice6();
+//		fp.practice7();
+//		fp.practice8();
+		fp.practice9();
+		
+		
+	}
+}
