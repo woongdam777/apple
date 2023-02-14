b94cc47996aa1ae49377a2946fe66d55d016cac2
woong <woongdam777@gmail.com>
2023-02-15 01:01:52 +0900

    배열실습문제풀기

--- 01_Java/a004_array/src/edu/kh/array/practice/ArrayPractice.java
+++ 01_Java/a004_array/src/edu/kh/array/practice/ArrayPractice.java
@@ -1,5 +1,7 @@
 package edu.kh.array.practice;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayPractice {
@@ -56,38 +58,137 @@ public class ArrayPractice {
 			System.out.print("입력 " + i +" : ");
 			arr4[i]=sc.nextInt();
 		}
-	    System.out.print("검색할 값 : ");
+		
+		System.out.print("검색할 값 : ");
 		int input = sc.nextInt();
-		boolean flag = ture;
+		
+		boolean flag = true;
+		int target = 0;
 		
 		for(int i=0;i<arr4.length;i++) {
-			if(ture) {
-				System.out.print("인덱스 : " + i);
-				
+			if(input == arr4[i]) {
+				target = i;
+				flag = false;
+				break;
 			}
-			System.out.println("일치하는 값이 존재하지 않습니다.");
 		}	
-		
-
+		if(flag) {
+			System.out.println("일치하는 값이 존재하지 않습니다.");
+			
+		}else {
+			System.out.print("인덱스 : " + target);
+			
+		}
 	}
 	
 	
 	public void practice5() {
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("문자열 : ");
+		String chr = sc.next();
+				
+		System.out.print("문자 : ");
+		char input = sc.next().charAt(0);
+		
+		int index = 0;
+		int num = 0;
+
+		char[] arr = new char[chr.length()];
+		
+		System.out.printf("%s에 %s가 존재하는 위치(인덱스) : ",chr,input);				
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=chr.charAt(i);
+			
+			if(arr[i] == input) {
+				System.out.print(i + " ");
+				num++;
+			}
+			
+		}
+		System.out.println();
+		System.out.printf("%s 개수 : %d",input,num);
+
 	}
-	
-	
+				
 	public void practice6() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 : ");
+		int input = sc.nextInt();
+		int[] arr = new int[input];
+		int sum = 0;
 		
+		for(int i=0;i<arr.length;i++) {
+			System.out.printf("배열 %d번째 인덱스에 넣을 값 : ",i);
+			arr[i]=sc.nextInt();
+			sum +=arr[i];
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i] + " ");
+			
+		}
+		System.out.println();
+		System.out.println("총 합 : " + sum);
 	}
 	
 	
 	public void practice7() {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("주민등록번호(-포함) : ");
+		String num = sc.next();
+		char[] arr= new char[num.length()];
+		for(int i=0; i<arr.length ; i++) {
+			arr[i]=num.charAt(i);
+			if(i>7) {
+				arr[i]='*';
+			}
+			System.out.print(arr[i]);
+		}
 	}
 	
 	
 	public void practice8() {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("정수 : ");
+		int input = sc.nextInt();
+		int[] arr = new int[input];
+		for(int i=0;i<(int)(arr.length/2);i++) {
+		
+			arr[i]=i+1;
+			System.out.print(arr[i]+", ");
+		}
+		for(int i=(int)(arr.length/2+1); i>0;i--) {
+			arr[i]=i;
+			System.out.print(arr[i]+", ");
+		}
+		
+//		if(input%2 != 1 || input < 3) {
+//			System.out.print("다시 입력하세요.");
+//			continue;
+//		}
+		
+	}
+	
+	public void practice9() {
+		
+	}
+	
+	public void practice10() {
+		
+	}
+	
+	public void practice11() {
+		
+	}
+	
+	public void practice12() {
+		
+	}
+	
+	public void practice13() {
 		
 	}
 	
--- 01_Java/a004_array/src/edu/kh/array/practice/ArrayPracticeRun.java
+++ 01_Java/a004_array/src/edu/kh/array/practice/ArrayPracticeRun.java
@@ -8,7 +8,11 @@ public class ArrayPracticeRun {
 //		ar.practice1();
 //		ar.practice2();
 //		ar.practice3();
-		ar.practice4();
+//		ar.practice4();
+//		ar.practice5();
+//		ar.practice6();
+//		ar.practice7();
+		ar.practice8();
 		
 	}
 }
