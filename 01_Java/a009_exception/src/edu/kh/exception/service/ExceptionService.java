e65890a8db4dd869dcb39ab7f09616ad722659d6
woong <woongdam777@gmail.com>
2023-02-23 15:54:40 +0900

    20230223_예외처리

--- 01_Java/a009_exception/src/edu/kh/exception/service/ExceptionService.java
+++ 01_Java/a009_exception/src/edu/kh/exception/service/ExceptionService.java
@@ -3,13 +3,14 @@ package edu.kh.exception.service;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class ExceptionService {
 	
-	public void ex1() throws IOException {
+	public void ex1() throws IOException  {
 					// 해당 메서드 내에서 IOException이 발생할 것을 대비한 예외처리 코드
 		
-		// 예외(Exception)화깅ㄴ하기
+		// 예외(Exception)확인하기
 		
 		// 키보드 입력을 효율적으로 읽어오는 객체
 		// (Scanner보다 기능은 부족하지만 속도는 빠름)
@@ -25,5 +26,119 @@ public class ExceptionService {
 		
 		
 		System.out.println("입력값 : " + input);
+		
+		// Checked Exception : 
+		// 컴파일 단계에서 예외가 발생할 가능성이 있는지
+		// 반드시 확인해야하는 예외
+		
+		// -> 공식 API 문서의 메서드 설명에
+		//   throws OOOException으로 작성되어있는 메서드가 있으면
+		//	 해당 코드 사용시 문제가 발생할 것이다라고 생각하고
+		//	 그 상황에 대한 예외 처리 코드를 꼭 작성
+		
+		
+		
+		// Unchecked Exception : 
+		// 컴파일 단계에서 예외가 발생할 가능성이 있는지 확인하지 않는 예외
+		
+		// -> 개발자의 부주의로 나타나는 예외
+		// --> 대부분 쉽게 해결(if문 사용)
+		
+		// -> 치명적인 문제가 아님
+		
+		System.out.println(5/0);
+		
+		int[] arr = new int[4];
+		System.out.println(arr[10]);
+		
+		String s = null;
+		System.out.println(s.equals("bbb"));
+		
 	}
+	
+	public void ex2() {
+		// 예외(Exception) : 코드 수정으로 해결 가능한 에러
+		// 예외 처리 : 예외를 처리할 수 있는 구문
+		
+		// [예외 처리 1] try ~ catch ~ finally
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// try : {} 내부에 예외가 발생할 것 같은 코드를 작성하고 실행 시도
+		try {
+		
+		
+		
+			System.out.print("입력 : ");
+			String input = br.readLine();
+			// public String readLine()  throws IOException
+			// -> IOException 이라는 Checked Exception 을 발생 시키는 메서드
+			//  --> Exception이 발생할 경우에 대비해서 예외처리 구문 작성(강제)
+			
+			/* 예외 강제 발생*/
+			//-> 예외 객체를 새로 만들어서 던짐
+			
+			throw new IOException(); 
+			
+			
+		} catch(IOException e) {
+			// catch : try 구문 내에서 던져진 예외가 있을 경우
+			//			해당 에외 객체를 잡아채서 catch 구문을 수행해 처리
+			
+			// catch의 매개 변수에는 던져진 예외 객체를 저장할 수 있는 참조 변수를 작성
+		
+			System.out.println("키보드 문제로 입력을 진행할 수 없습니다.");
+			
+			// 발생된 예외가 처리된 후
+			// 프로그램이 종료되지 않고 다음 코드가 수행됨
+		}
+		
+		System.out.println("try-catch가 수행되도 프로그램이 종료되지 않음");
+		
+	}
+	
+	public void ex3() {
+		
+		// 입력 받은 두 정수 나누기
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			
+			System.out.print("입력 1 : ");
+			int num1 = sc.nextInt();
+			
+			System.out.print("입력 2 : ");
+			int num2 = sc.nextInt();
+			
+			System.out.printf("%d / %d = %d \n", num1, num2, num1/num2);
+			
+		}catch(ArithmeticException e) {
+			// 산술적 예외를 잡아 처리
+			System.out.println("0으로 나눌 수 없습니다.");
+		}finally {
+			// finally
+			// try-catch 구문이 끝난 후 마지막으로 수행
+			// ** 예외가 발생하든말든 무조건 실행 **
+			System.out.println("프로그램 종료");
+			
+			
+		}
+		
+		
+		/* if로 간단하게 처리할 수 있음
+		 * if(num2 == 0){
+		 * 	 System.out.println("0으로 나눌 수 없습니다.");
+		 * }else{
+		 * 	 System.out.printf("%d / %d = %d \n", num1, num2, num1/num2);
+		 * }
+		 * */
+		
+		
+	}
+	
+	
+	
+	
 }
+
+
