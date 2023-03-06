ba70d7ddd776710236f3c7ac5a199d5d176d34e5
woong <woongdam777@gmail.com>
2023-03-06 17:25:09 +0900

    20230306_DB세팅시작_계정생성_연산자

---
+++ 02_DB/00_kh_kjw_생성(sys).sql
@@ -0,0 +1,19 @@
+-- 한 줄 주석
+
+/*
+범위
+주석
+*/
+
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+-- CTRL + ENTER : 선택한 SQL 수행
+
+-- 사용자 계정 생성
+CREATE USER kh_kjw IDENTIFIED BY "kjaew_1234";
+
+-- 사용자 계정에 권환 부여
+GRANT RESOURCE, CONNECT TO kh_kjw;
+
+-- 객체가 생성될 수 잇는 공간 할당량 지정
+ALTER USER kh_kjw DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+
