0b49f506f711dc3868586f3b4d928fec722eaaca
woongdam <woongdam777@gmail.com>
2023-04-19 16:35:16 +0900

    20230419_semiproject

--- semi_project/mainFrame.js
+++ semi_project/mainFrame.js
@@ -1,11 +1,24 @@
 document.getElementById("startPetitions").addEventListener("click",function () {
-    document.getElementById("mf").innerHTML = '<iframe class="myPetitions" src="startPetitions/startPetitions.html"></iframe>';
+    document.getElementById("mf").innerHTML = '<iframe id="iframe" onload="iframeLoaded()" frameborder = "0" src="startPetitions/startPetitions.html"></iframe>';
 });
 
 document.getElementById("myPetitions").addEventListener("click",function () {
-  document.getElementById("mf").innerHTML = '<iframe class="myPetitions" src="myPetitions/myPetitions.html"></iframe>';
+  document.getElementById("mf").innerHTML = '<iframe id="iframe" onload="iframeLoaded()" frameborder = "0" src="myPetitions/myPetitions.html"></iframe>';
 });
 
 document.getElementById("browse").addEventListener("click",function () {
-  document.getElementById("mf").innerHTML = '<iframe class="myPetitions" src="browse/browser.html"></iframe>';
+  document.getElementById("mf").innerHTML = '<iframe id="iframe" onload="iframeLoaded()" frameborder = "0" src="browse/browser.html"></iframe>';
 });
+
+function iframeLoaded() {
+  let frame = document.getElementById('iframe');
+  let mheight = document.getElementById('mf');
+  let height = document.getElementById('iframe').contentWindow.document.body.scrollHeight;
+  let width = document.getElementById('iframe').contentWindow.document.body.scrollWidth;
+
+  mheight.height = height + "px";
+  mheight.width = width + "px";
+
+  frame.height = height + "px";
+  frame.width = width + "px"
+}
