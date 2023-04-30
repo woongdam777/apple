879d3b4f1f0c45c9f80c04ceef1d9dc16f4c8f3a
woong <woongdam777@gmail.com>
2023-05-01 01:19:33 +0900

    semi

---
+++ semi_project/vote/myPetitions/myPetitionsDashboard/myPetitionsDashboard.js
@@ -0,0 +1,3 @@
+document.getElementById("linkBtn").addEventListener("click", () => {
+  document.getElementById("linkChange").style.display = "block";
+});
--- semi_project/vote/myPetitions/myPetitionsEdit/myPetitionsEdit.js
+++ semi_project/vote/myPetitions/myPetitionsEdit/myPetitionsEdit.js
@@ -7,3 +7,44 @@ document.getElementById("descriptTipBtn").addEventListener("click", () => {
 document.getElementById("imgTipBtn").addEventListener("click", () => {
     document.getElementById("imgTip").style.display = "block";
 });
+
+
+document.getElementById("scopeI").addEventListener("mouseover", () =>{
+    document.getElementById("scopeP").style.display = "block";
+});
+
+document.getElementById("scopeI").addEventListener("mouseout", () =>{
+    document.getElementById("scopeP").style.display = "none";
+});
+
+document.getElementById("locationI").addEventListener("mouseover", () =>{
+    document.getElementById("locationP").style.display = "block";
+});
+
+document.getElementById("locationI").addEventListener("mouseout", () =>{
+    document.getElementById("locationP").style.display = "none";
+});
+
+document.getElementById("topicI").addEventListener("mouseover", () =>{
+    document.getElementById("topicP").style.display = "block";
+});
+
+document.getElementById("topicI").addEventListener("mouseout", () =>{
+    document.getElementById("topicP").style.display = "none";
+});
+
+document.getElementById("ownerI").addEventListener("mouseover", () =>{
+    document.getElementById("ownerP").style.display = "block";
+});
+
+document.getElementById("ownerI").addEventListener("mouseout", () =>{
+    document.getElementById("ownerP").style.display = "none";
+});
+
+document.getElementById("twitterI").addEventListener("mouseover", () =>{
+    document.getElementById("twitterP").style.display = "block";
+});
+
+document.getElementById("twitterI").addEventListener("mouseout", () =>{
+    document.getElementById("twitterP").style.display = "none";
+});
