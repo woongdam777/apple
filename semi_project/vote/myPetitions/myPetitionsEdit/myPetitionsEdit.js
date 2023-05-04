c17d6b0ff74b4cad2153d84c13e347cae90dcfa2
woongdam <woongdam777@gmail.com>
2023-05-04 17:50:14 +0900

    semi

--- semi_project/vote/myPetitions/myPetitionsEdit/myPetitionsEdit.js
+++ semi_project/vote/myPetitions/myPetitionsEdit/myPetitionsEdit.js
@@ -30,10 +30,24 @@ tipHover("topic");
 tipHover("owner");
 tipHover("twitter");
 
+/* 사진 또는 비디오 주소 추가 */
+document.getElementById("AddBtn").addEventListener("click", () => {
+    const addurl = document.getElementById("AddUrl").value;
+    document.querySelector(".content-img").innerHTML = "";
+    document.querySelector(".content-img").style = "height:300px"
 
+    const input = document.createElement("div");
 
+    const closeBtn = document.createElement("button");
+    closeBtn.innerText = "버튼";
+    closeBtn.style.position = "absolute";
+    closeBtn.style.backgroundColor = "red";
 
+    const youtube = document.createElement("iframe");
+    youtube.src = "https://www.youtube.com/embed/sH2pXruvCZI";
+    youtube.style.height = "300px";
 
+    input.append(closeBtn, youtube);
 
-
-
+    document.querySelector(".content-img").append(input);
+});
